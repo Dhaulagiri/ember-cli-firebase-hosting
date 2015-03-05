16f464e3ce17d26df7448d84abed06db89bccb73
Brian Runnells <brian.runnells@gmail.com>
2015-03-04 16:14:32 -0800

    Expose firebase command

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
 module.exports = {
-  name: 'ember-cli-firebase-hosting'
-};
+  name: 'ember-cli-firebase-hosting',
+
+  includedCommands: function() {
+    return {
+      'firebase': require('./lib/commands/firebase')
+    }
+  },
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  }
+}
