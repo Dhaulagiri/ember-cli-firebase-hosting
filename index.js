04e464b84fd220ab9a17de15a4ef5fbf9a49f993
Brian Runnells <brian.runnells@gmail.com>
2015-03-04 16:01:16 -0800

    Upgrade to ember-cli 0.2.0-beta.1

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
+/* jshint node: true */
 'use strict';
-var path = require('path');
 
 module.exports = {
-  name: 'ember-cli-firebase-hosting',
-
-  includedCommands: function() {
-    return {
-      'firebase': require('./lib/commands/firebase')
-    }
-  },
-
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
-  }
-}
+  name: 'ember-cli-firebase-hosting'
+};
